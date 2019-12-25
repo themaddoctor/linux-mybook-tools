980ca938812ce7b05cc763327674ecc6d65e8c50
themaddoctor <hippykitty@protonmail.com>
2019-12-24 21:05:02 -0500

    Add files via upload

--- wd_kdf.c
+++ wd_kdf.c
@@ -1,12 +1,20 @@
+/* wd_kdf.c
+   compile with gcc -o wd_kdf -lssl -lcrypto
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <iconv.h>
 #include <openssl/evp.h>
 #include <errno.h>
 
-main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 
+#if (OPENSSL_VERSION_NUMBER < 0x10100000L)
   EVP_MD_CTX mdctx;
+#else
+  EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
+#endif
   char *password, *password_copy;
   char *md_value, *md_value_copy;
   int md_len, i;
@@ -31,13 +39,23 @@ main(int argc, char *argv[]) {
   OpenSSL_add_all_digests();
 
   for (i=0;i<1000;i++) {
+#if (OPENSSL_VERSION_NUMBER < 0x10100000L)
     EVP_MD_CTX_init(&mdctx);
     EVP_DigestInit_ex(&mdctx, EVP_sha256(), NULL);
     EVP_DigestUpdate(&mdctx, md_value, md_len);
     EVP_DigestFinal_ex(&mdctx, md_value, &md_len);
     EVP_MD_CTX_cleanup(&mdctx);
+#else
+    EVP_DigestInit_ex(mdctx, EVP_sha256(), NULL);
+    EVP_DigestUpdate(mdctx, md_value, md_len);
+    EVP_DigestFinal_ex(mdctx, md_value, &md_len);
+#endif
     }
+#if (OPENSSL_VERSION_NUMBER >= 0x10100000L)
+  EVP_MD_CTX_free(mdctx);
+#endif
 
   for(i = 0; i < md_len; i++) printf("%02x", md_value[i] & 0xff);
   printf("\n");
+  return 0;
   }
