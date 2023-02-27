dd78d667b12f570c9dbb9157ec2f493d77ef5438
themaddoctor <insanitas@proton.me>
2023-02-26 20:36:10 -0500

    Add files via upload

--- oxuf943se-extract-DEK-from-EEPROM-keyblock.sh
+++ oxuf943se-extract-DEK-from-EEPROM-keyblock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# usage: bash oxuf943se-extract-DEK-from-keyblock.sh <kek file (hex)> <keyblock file (binary)>
+# usage: bash oxuf943se-extract-DEK-from-EEPROM-keyblock.sh <kek file (hex)> <keyblock file (binary)>
 
 KEK="$1"
 KEYBLOCK="$2"
@@ -7,9 +7,11 @@ TEMPDIR=`mktemp -d ./WDXXXXXXXX`
 
 cat "$KEK" | grep -o ........ | tac | echo "$(tr -d '\n')" > $TEMPDIR/kek1.hex
 
-dd if="$KEYBLOCK" bs=4 skip=5 count=64 of=$TEMPDIR/kb1.bin status=none
+dd if="$KEYBLOCK" bs=2 skip=9 count=32 of=$TEMPDIR/kb1.bin status=none
 openssl enc -d -aes-256-ecb -K `cat $TEMPDIR/kek1.hex` -nopad \
     -in $TEMPDIR/kb1.bin -out $TEMPDIR/kb2.bin
+
+# hexdump -C $TEMPDIR/kb2.bin
 
 checksum=`dd if=$TEMPDIR/kb2.bin bs=16 skip=3 count=1 status=none | md5sum | cut -d ' ' -f 1`
 if [ "$checksum" != "4ae71336e44bf9bf79d2752e234818a5" ]; then
